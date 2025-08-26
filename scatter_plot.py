8de44cf665e82430acae3bb802525768a68638f5
Franck Li <fli@f6r9s11.clusters.42paris.fr>
2025-08-26 18:01:03 +0200

    scatter plot find most correlated

---
+++ scatter_plot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import utils
+import argparse
+import math
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from describe import ColumnStats
+
+def main():
+    parser = argparse.ArgumentParser(description="Describe dataset")
+    parser.add_argument("dataset", type=str, help="dataset to describe")
+    args = parser.parse_args()
+
+    df = utils.load_csv(args.dataset)
+    if df is None or df.empty:
+        print("Empty or invalid dataset.")
+        return
+    del df['Index']
+
+    numeric_df = df.select_dtypes(include=["number"])
+    if numeric_df.empty:
+        print("No numerical features found.")
+        return
+
+    matrix  = numeric_df.corr().abs()
+    # plt.figure(figsize=(8,6))
+    # sns.heatmap(matrix, annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+    # plt.title("Correlation Heatmap")
+    # plt.show()
+
+    def get_redundant_pairs(df):
+        '''Get diagonal and lower triangular pairs of correlation matrix'''
+        pairs_to_drop = set()
+        cols = df.columns
+        for i in range(0, df.shape[1]):
+            for j in range(0, i+1):
+                pairs_to_drop.add((cols[i], cols[j]))
+        return pairs_to_drop
+
+    def get_top_abs_correlations(df, n=5):
+        au_corr = df.corr().abs().unstack()
+        labels_to_drop = get_redundant_pairs(df)
+        au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
+        return au_corr[0:n]
+
+    top_correlation_series = get_top_abs_correlations(numeric_df, 1)
+    most_correlated_pair = top_correlation_series.index[0]
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
